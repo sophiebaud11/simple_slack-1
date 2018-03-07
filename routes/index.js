493e7884ef9c173bd6a4ecd5c1dbd6e8a7dcd39b
mk <mk@mks-MacBook-Pro-2.local>
2018-03-06 21:36:29 -0500

    payload change

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post('/simple-slack-slash', function(req, res, next) {
   console.log(JSON.stringify(req.body, null, 4));
   if (req.body.user_name) {
     var theText = 'just received a message from user ' + req.body.user_name + ": \n" + req.body.text + ".\n\n We'll do more interesting stuff in a bit."
-    res.send(theText);
+    res.JSON({"text":theText});
     // {"text": theText, "attachments": ["title":"LL gif", "image_url":"http://codelab.learninglab.xyz/gifs/mk_artifact.gif"]}
   }
   else {
