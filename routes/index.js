5d222124765f1187b8f4e05ff797cc808f745203
mk <mk@mks-MacBook-Pro-2.local>
2018-03-06 21:46:15 -0500

    payload change

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,24 @@ router.post('/simple-slack-slash', function(req, res, next) {
   console.log(JSON.stringify(req.body, null, 4));
   if (req.body.user_name) {
     var theText = 'just received a message from user ' + req.body.user_name + ": \n" + req.body.text + ".\n\n We'll do more interesting stuff in a bit."
-    var thePayload = {"text": theText, "attachments": ["title":"LL gif", "image_url":"http://codelab.learninglab.xyz/gifs/mk_artifact.gif"]}
+    var thePayload = {
+      "text": theText,
+      "attachments":
+        {
+          "title": "LL gif",
+          "image_url": "http://codelab.learninglab.xyz/gifs/mk_artifact.gif"
+        }
+      }
     console.log(JSON.stringify(thePayload, null, 4));
     res.send(JSON.stringify(thePayload));
+    res.send({
+      "text": theText,
+      "attachments":
+        {
+          "title": "LL gif",
+          "image_url": "http://codelab.learninglab.xyz/gifs/mk_artifact.gif"
+        }
+      });
 
   }
   else {
