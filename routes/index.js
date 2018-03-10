72e386a18a76c78882c8ec6b39a95f06c038f6e7
mk <mk@mks-MacBook-Pro-2.local>
2018-03-10 09:14:01 -0500

    testing to start

--- routes/index.js
+++ routes/index.js
@@ -86,7 +86,7 @@ router.post('/shootid-slash', function(req, res, next) {
   console.log("got a request:");
   console.log(JSON.stringify(req.body, null, 4));
   res.send('just received a message. will do more soon');
-  web.chat.postMessage({ channel: req.body.channel, text: 'Hello there' })
+  web.chat.postMessage({ channel: req.body.channel_id, text: 'Hello there' })
   .then((res) => {
     // `res` contains information about the posted message
     console.log('Message sent: ', res.ts);
@@ -104,34 +104,34 @@ router.post('/slack-events', function(req, res){
   //     console.log("saved event to db");
   //   }
   // })
-  console.log(JSON.stringify(req.body));
-  web.chat.postMessage({
-  channel: req.body.channel,
-  text: 'Hello there',
-  attachments: [
-    {
-      "fallback": "Required plain-text summary of the attachment.",
-      "color": "#36a64f",
-      "author_name": "Shoot ID Bot",
-      "author_link": "http://flickr.com/bobby/",
-      "author_icon": "http://flickr.com/icons/bobby.jpg",
-      "title": "Slack API Documentation",
-      "title_link": "https://api.slack.com/",
-      "text": "Optional text that appears within the attachment",
-      "fields": [
-        {
-          "title": "Priority",
-          "value": "High",
-          "short": false
-        }
-      ],
-      "image_url": "http://my-website.com/path/to/image.jpg",
-      "thumb_url": "http://example.com/path/to/thumb.png",
-      "footer": "Slack API",
-      "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-      "ts": 123456789
-    }
-  ]
+  console.log('just got an event \n' + JSON.stringify(req.body));
+  // web.chat.postMessage({
+  // channel: req.body.channel,
+  // text: 'Hello there',
+  // attachments: [
+  //   {
+  //     "fallback": "Required plain-text summary of the attachment.",
+  //     "color": "#36a64f",
+  //     "author_name": "Shoot ID Bot",
+  //     "author_link": "http://flickr.com/bobby/",
+  //     "author_icon": "http://flickr.com/icons/bobby.jpg",
+  //     "title": "Slack API Documentation",
+  //     "title_link": "https://api.slack.com/",
+  //     "text": "Optional text that appears within the attachment",
+  //     "fields": [
+  //       {
+  //         "title": "Priority",
+  //         "value": "High",
+  //         "short": false
+  //       }
+  //     ],
+  //     "image_url": "http://my-website.com/path/to/image.jpg",
+  //     "thumb_url": "http://example.com/path/to/thumb.png",
+  //     "footer": "Slack API",
+  //     "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+  //     "ts": 123456789
+  //   }
+  // ]
 })
   .then((res) => {
     // `res` contains information about the posted message
